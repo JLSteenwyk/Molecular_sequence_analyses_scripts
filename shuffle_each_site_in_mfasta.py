5adaec20d33306e79cf7c6281e8512bcecb1a753
Jacob Steenwyk <jacob.steenwyk@vanderbilt.edu>
2018-02-20 21:13:21 -0600

    finished shuffle script

--- shuffle_each_site_in_mfasta.py
+++ shuffle_each_site_in_mfasta.py
@@ -6,13 +6,11 @@ import os.path
 import re
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
-from Bio.Alphabet import IUPAC
 from Bio.Seq import Seq
-import numpy as np
-from datetime import datetime
+import random
 
 def shuffle(
-    fasta, sequence_type
+    fasta, sequence_type, seed
     ):
     """
     Reads the alignment list and taxa list
@@ -23,6 +21,8 @@ def shuffle(
         input fasta file
     argv: sequence_type
         specifies sequence type
+    argv: seed
+    	pseudorandomizes sequences
     """
 
     # read in fasta file to alignment variable
@@ -30,8 +30,9 @@ def shuffle(
     handle    = open(fasta)
     alignment = list(SeqIO.parse(handle, format))
 
-    # initialize step size
-    step = 1
+    # initialize step and window size
+    step   = 1
+    window = 1
 
     # create a variable with all taxa names
     taxa_list = []
@@ -46,19 +47,40 @@ def shuffle(
             seqDict[indiv.id] = indiv.seq
 
     # determine length of sequence
-    length    = len(alignment[0].seq)
+    length  = len(alignment[0].seq)
+
+    # initialize output dictionary and populate with taxa names
+    outDict = {}
+    for indiv in alignment:
+        if indiv.id in taxa_list:
+            outDict[indiv.id] = ''
+
+    # set seed
+    random_numbers = []
+    random.seed(seed)
+    for x in range(length):
+        random_numbers.append(random.randint(1,9999999999))
 
     # loop through aligned sequence using step size
-    for i in range(0, (int(length)+1) - int(step), int(step)):
-        # initialize variables to hold sequence of interest being analyzed
-        clade1seg   = ''
-        cladeOutseg = ''
-        # loop through all individuals from clade of interest and save 
-        # window being analyzed into a concatenated string from all indivs
-        for k, v in clade1D.items():
-            clade1seg   += (v[i:i+int(window)])
-        for k, v in cladeOutD.items():
-            cladeOutseg += (v[i:i+int(window)])
+    for i, randoNum in zip(range(0, (int(length)+1) - int(step), int(step)), random_numbers):
+        # initialize variables to hold sequence at position
+        positionSeq = []
+        # loop through all individuals and save 
+        # window being analyzed into a list
+        for k, v in seqDict.items():
+            positionSeq.append(v[i:i+int(window)])
+        
+        # shuffle the position of the nucleotides
+        random.seed(randoNum)
+        random.shuffle(positionSeq)
+        
+        # append the shuffled sequence to the value string
+        for indiv, sequence in zip(taxa_list, positionSeq):
+            outDict[indiv]+=sequence
+
+    # print results to a fasta file format
+    for indiv in taxa_list:
+        print(">"+indiv+"\n"+outDict[indiv])
 
 def main(
     argv
@@ -67,16 +89,13 @@ def main(
     Reads arguments 
     """
 
-    # start time of script
-    startTime = datetime.now()
-
     # initialize argument variables
     alignment_list  = ''
     taxa_list       = ''
     prefix          = ''
 
     try:
-        opts, args = getopt.getopt(argv, "hi:t:")
+        opts, args = getopt.getopt(argv, "hi:t:s:")
     except getopt.GetoptError:
         # error message
         print("Error\nFor help use -h argument\n")
@@ -98,12 +117,15 @@ def main(
             print("test for clonality -- see figure 4 in http://jcm.asm.org/content/41/2/703.full")
             ## options
             # fasta file
-            print("\n-i <input fasta file>")
+            print("\n-i <input fasta file>:")
             print("\tSingle column file of the alignment files that will be concatenated.")
             # specify if nucleotide or protein files list
-            print("\n-t <fasta files are either nucleotide or protein fastas>")
+            print("\n-t <fasta files are either nucleotide or protein fastas>:")
             print("\tArgument can be either 'prot' or 'nucl' to specify if the")
-            print("\talignments contain protein or nucleotide sequences.\n")
+            print("\talignments contain protein or nucleotide sequences.")
+            # seed integer
+            print("\n-s <seed integer>:")
+            print("\tset seed for psuedorandomization of sequences\n")
             sys.exit()
 
         elif opt == '-i':
@@ -122,10 +144,16 @@ def main(
                 print("\n\nThe specified input for (-t) should be 'prot' or 'nucl'.\n")
                 print("For detailed explanation of configuration file use -h argument\n")
                 sys.exit()
+        elif opt == '-s':
+            if int(arg):
+                seed = arg
+            else:
+                # error message
+                print("\n\nThe specified input for (-s) should be an integer.\n")
 
     # pass to shuffle function
     shuffle(
-        fasta, sequence_type
+        fasta, sequence_type, seed
         )
 
 if __name__ == '__main__':
